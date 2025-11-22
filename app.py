ef154089bb581ec03c1ac437648fa3d42b494021
Azkill <jaderjavierrinconcaicedo01@gmail.com>
2025-11-22 11:53:08 -0500

    commit 6

--- app.py
+++ app.py
@@ -15,6 +15,8 @@ def create_app():
     
     return app
 
+# 👇 ESTA LÍNEA ES LA QUE HACE FALTA PARA RENDER / GUNICORN
+app = create_app()
+
 if __name__ == "__main__":
-    app = create_app()
     app.run(debug=True)
